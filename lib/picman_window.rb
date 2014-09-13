e81ed493428d35d8b6c290ab6bdf31b335ba17b3
David Vilar <davidv@shutl.com>
2014-09-13 23:10:18 +0200

    rendering a background

--- lib/picman_window.rb
+++ lib/picman_window.rb
@@ -1,12 +1,15 @@
 class PicmanWindow < Gosu::Window
   def initialize
-    super 640, 480, false
+    super(800, 600, false)
     self.caption = "Gosu Tutorial Game"
+
+    @background_image = Gosu::Image.new(self, "media/space.png", true)
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end
 end
