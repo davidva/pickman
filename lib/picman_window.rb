3b53933c25ea6c206d592abab1adf3df5e748353
David Vilar <davidv@shutl.com>
2014-09-13 23:24:22 +0200

    adding starfighter

--- lib/picman_window.rb
+++ lib/picman_window.rb
@@ -4,12 +4,24 @@ class PicmanWindow < Gosu::Window
     self.caption = "Gosu Tutorial Game"
 
     @background_image = Gosu::Image.new(self, "media/space.png", true)
+    @player = Player.new(self)
+    @player.warp(320, 240)
   end
 
   def update
+    @player.turn_left  if button_down?(Gosu::KbLeft) || button_down?(Gosu::GpLeft)
+    @player.turn_right if button_down?(Gosu::KbRight) || button_down?(Gosu::GpRight)
+    @player.accelerate if button_down?(Gosu::KbUp) || button_down?(Gosu::GpButton0)
+
+    @player.move
   end
 
   def draw
+    @player.draw
     @background_image.draw(0, 0, 0)
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
   end
 end
